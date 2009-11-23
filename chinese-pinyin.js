e985e502469535f9efa43f3b735c852361abd271
ZhangJinzhu <wosmvp@gmail.com>
2009-11-23 10:14:50 +0800

    update chinese-pinyin.js, 使用中文首字符匹配

--- chinese-pinyin.js
+++ chinese-pinyin.js
@@ -6,13 +6,12 @@ function CC2PY(l1) {
 	var reg = new RegExp('[a-zA-Z0-9\- ]');
 	for (var i=0;i<l2;i++) {
 		var val = l1.substr(i,1);
-		var name = arraySearch(val,PinYin);
 		if(reg.test(val)) {
 			I1 += val;
-		} else if (name!==false) {
-			I1 += name;
+		} else {
+      var name = arraySearch(val,PinYin);
+			if(name!==false){ I1 += name.substr(0,1); }
 		}
-		
 	}
 	I1 = I1.replace(/ /g,'-');
 	while (I1.indexOf('--')>0) {
