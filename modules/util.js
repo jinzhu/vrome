deec71a9b3c77f3ce9842a4644e10e6b3f33afe2
ZhangJinzhu <wosmvp@gmail.com>
2009-12-13 18:29:12 +0800

    utils

---
+++ modules/util.js
@@ -0,0 +1,73 @@
+/**
+ * Util
+ */
+
+var Util = new Object();
+
+(function(){
+
+	function focusFirstTextInput(){
+		var elem = document.querySelector('input[type="text"],input:not([type])');
+		if (elem) {
+			elem.focus();
+			elem.setSelectionRange(0,elem.value.length);
+		}
+		keyListener({add : initKeyBind,remove : gHandler});
+	}
+
+	function blurFocus(){
+		document.activeElement.blur();
+	}
+
+	function moveFirstOrSelectAll(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		if (caret_position == 0){
+			elem.setSelectionRange(0, elem.value.length); // select all text
+		}else{
+			elem.setSelectionRange(0, 0);
+		}
+	}
+
+	function moveEnd(){
+		var elem = document.activeElement;
+		elem.setSelectionRange(elem.value.length, elem.value.length);
+	}
+
+	function moveForward(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		elem.setSelectionRange(caret_position + 1, caret_position + 1);
+	}
+
+	function moveBackward(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		elem.setSelectionRange(caret_position - 1, caret_position - 1);
+	}
+
+	function deleteForward(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		var org_str = elem.value;
+		elem.value = org_str.substring(0, caret_position) + org_str.substring(caret_position + 1, org_str.length);
+		elem.setSelectionRange(caret_position, caret_position);
+	}
+
+	function deleteBackward(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		var org_str = elem.value;
+		elem.value = org_str.substring(0, caret_position - 1) + org_str.substring(caret_position, org_str.length);
+		elem.setSelectionRange(caret_position - 1, caret_position - 1);
+	}
+
+	function deleteBackwardWord(){
+		var elem = document.activeElement;
+		var caret_position = elem.selectionEnd;
+		var org_str = elem.value;
+		elem.value = org_str.substring(0, caret_position - 1).replace(/\S*\s*$/,'') + org_str.substring(caret_position, org_str.length);
+		var position = elem.value.length - (org_str.length - caret_position);
+		elem.setSelectionRange(position,position);
+	}
+})()
