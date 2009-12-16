26460a770daf183667bc1a5c6049a4a063382059
ZhangJinzhu <wosmvp@gmail.com>
2009-12-16 23:05:43 +0800

    DOM based search,much faster

--- modules/search.js
+++ modules/search.js
@@ -1,68 +1,66 @@
 var Search = (function(){
-    var className    = "__vimlike_highlight_box";
-    var startTag     = "<span class='__vimlike_highlight_box'>";
-    var endTag       = "</span>";
-    var originalBody;
+  var highlight_class = '__vimlike_search_highlight';
 
-    function find(keyword) {
-      if(!keyword) return;
-      var newBody  = "";
-      var bodyText = document.body.innerHTML;
-      if(!originalBody) originalBody = bodyText;
-      var k = keyword.toLowerCase();  // Lower Case Keyword
-      var b = bodyText.toLowerCase(); // Lower Case Body Text
+  function find(keyword,node) {
+    if(!keyword) return;
+    if(!node) node = document.body;
 
-      var i = -1;
-      var j = -1;
+    // Iterate node childNodes
+    if (node.hasChildNodes() && !/(script|style)/i.test(node.tagName)) {
+      for (var i = 0;i < node.childNodes.length;i++) {
+        find(keyword, node.childNodes[i]);
+      }
+    }
 
-      while (bodyText.length > i) {
-        j = i;
-        i = b.indexOf(k, i);
+    if (node.nodeType == 3) { // text node
+      var tmpKey = keyword.toUpperCase();
+      var tmpText = node.data.toUpperCase();
+      var ni = tmpText.indexOf(tmpKey);
+      if (ni != -1) {
+        var pn = node.parentNode;
 
-        if (i < 0) {
-          newBody += bodyText.substr(j);
-          break;
-        } else {
-          var c_tag_index    = bodyText.lastIndexOf(">", i);
-          var o_tag_index    = bodyText.lastIndexOf("<", i);
-          var c_script_index = bodyText.lastIndexOf("/script>", i);
-          var o_script_index = bodyText.lastIndexOf("<script", i);
-          var c_style_index  = bodyText.lastIndexOf("/style>", i);
-          var o_style_index  = bodyText.lastIndexOf("<style", i);
+        if (pn.className != highlight_class) {
+          var nv = node.data;
 
-          // skip anything inside an html/script/style tag
-          if (c_tag_index >= o_tag_index && c_script_index >= o_script_index && c_script_index >= o_style_index) {
-            newBody += bodyText.substring(j, i) + startTag + bodyText.substr(i, keyword.length) + endTag;
-          }else{
-            newBody += bodyText.substring(j, i + keyword.length);
-          }
+          var before = document.createTextNode(nv.substr(0,ni));
+          var match  = document.createTextNode(nv.substr(ni,keyword.length));
+          var after  = document.createTextNode(nv.substr(ni+keyword.length));
 
-          i = i + keyword.length;
+          var hiLabel = document.createElement("span");
+          hiLabel.className = highlight_class;
+          hiLabel.appendChild(match);
+          pn.insertBefore(before,node);
+          pn.insertBefore(hiLabel,node);
+          pn.insertBefore(after,node);
+          pn.removeChild(node);
         }
       }
-  
-      document.body.innerHTML = newBody;
     }
+  }
 
-    function handleInput(e){
-      remove();
-      find(CmdLine.get().content);
-    }
-
-    function remove() {
-      if(originalBody){
-        document.body.innerHTML = originalBody;
-        originalBody = '';
+  function remove() {
+    var nodes = document.getElementsByClassName(highlight_class);
+    var length = nodes.length;
+    for(var i = 0; i < length; i++){
+      if(nodes[0]){
+        var pn = nodes[0].parentNode;
+        pn.innerHTML = pn.innerText;
       }
     }
- 
-    function start(){
-      CmdLine.set({title : 'SearchMode',pressUp : handleInput,content : ''});
-      document.getElementById('__vimlike_cmd_input_box').focus();
-    }
+  }
 
-    return {
-      start  : start,
-      remove : remove,
-    }
+  function handleInput(e){
+    remove();
+    find(CmdLine.get().content);
+  }
+
+  function start(){
+    CmdLine.set({title : 'SearchMode',pressUp : handleInput,content : ''});
+    document.getElementById('__vimlike_cmd_input_box').focus();
+  }
+
+  return {
+    start  : start,
+    remove : remove
+  }
 })()
