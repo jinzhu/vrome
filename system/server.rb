e7f116afadacffa1256f24134219dc132159dd0e
ZhangJinzhu <wosmvp@gmail.com>
2009-12-27 13:22:07 +0800

    add server.rb

---
+++ system/server.rb
@@ -0,0 +1,19 @@
+require 'socket'
+require 'tempfile'
+
+server = TCPServer.new('localhost', 20000)
+
+while (session = server.accept)
+  # GET /?data='originaltext' HTTP/1.1
+  text = session.gets.split[1].split('=',2)[1]
+  puts "Request: text : #{text}"
+
+  tmpfile = Tempfile.new('editor')
+  tmpfile.write text
+  tmpfile.flush
+  system("gvim -f #{tmpfile.path}")
+  session.puts File.read(tmpfile.path)
+
+  tmpfile.delete
+  session.close
+end
