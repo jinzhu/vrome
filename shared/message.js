59d7adbe5bb02cb5a17d1ea08281112937625dca
ZhangJinzhu <wosmvp@gmail.com>
2010-01-11 14:57:03 +0800

    let's remove the ugly type error first

--- shared/message.js
+++ shared/message.js
@@ -1,26 +1,28 @@
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
-    var tab = port.tab;
-		var actions = msg.action.split('.');
-		var action  = window[actions.shift()];
+    try {
+      var tab = port.tab;
+      var actions = msg.action.split('.');
+      var action  = window[actions.shift()];
 
-		while (action && actions[0]) { action = action[actions.shift()]; }
+      while (action && actions[0]) { action = action[actions.shift()]; }
 
-    // if defined msg.arguments, then use it as arguments, else use msg.
-    if(typeof msg.arguments != 'undefined') {
-      var argument = (msg.arguments instanceof Array) ? msg.arguments : [msg.arguments];
-    } else {
-      var argument = [msg];
-    }
+      // if defined msg.arguments, then use it as arguments, else use msg.
+      if(typeof msg.arguments != 'undefined') {
+        var argument = (msg.arguments instanceof Array) ? msg.arguments : [msg.arguments];
+      } else {
+        var argument = [msg];
+      }
 
-    if (typeof Debug == 'function') {
-      Debug("Actions: " + msg.action + " Arguments: " + msg.arguments);
-    } else {
-      console.log("Actions: " + msg.action + " Arguments: " + msg.arguments);
-    }
+      if (typeof Debug == 'function') {
+        Debug("Actions: " + msg.action + " Arguments: " + msg.arguments);
+      } else {
+        console.log("Actions: " + msg.action + " Arguments: " + msg.arguments);
+      }
 
-    argument[argument.length] = tab;
+      argument[argument.length] = tab;
 
-		if(action instanceof Function) action.apply('', argument);
+      if(action instanceof Function) action.apply('', argument);
+    } catch(e) { }
   });
 })
