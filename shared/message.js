bf047eac3c586cbd760ad11bfd43ce554e772d9c
ZhangJinzhu <wosmvp@gmail.com>
2010-01-14 22:12:31 +0800

    debug

--- shared/message.js
+++ shared/message.js
@@ -1,7 +1,7 @@
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
     try {
-      var tab = port.tab;
+      var tab     = port.tab;
       var actions = msg.action.split('.');
       var action  = window[actions.shift()];
 
@@ -14,11 +14,7 @@ chrome.extension.onConnect.addListener(function(port) {
         var argument = [msg];
       }
 
-      if (typeof Debug == 'function') {
-        Debug("Actions: " + msg.action + " Arguments: " + msg.arguments);
-      } else {
-        console.log("Actions: " + msg.action + " Arguments: " + msg.arguments);
-      }
+      Debug("Actions: " + msg.action + " Arguments: " + msg.arguments);
 
       argument[argument.length] = tab;
 
