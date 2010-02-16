2617c45b841dfc27fc53bf72c7e4cf5de79ba3f1
ZhangJinzhu <wosmvp@gmail.com>
2010-02-16 23:44:14 +0800

    shorturl

--- src/background/scripts/main.js
+++ src/background/scripts/main.js
@@ -52,29 +52,6 @@ function externalEditor(msg) {
   xhr.send("{'method':'open_editor','editor':'" + (Settings.get('editor') || 'gvim') + "', 'data' : '" + msg.data + "'}");
 }
 
-function shortUrl(msg) {
-  var tab = arguments[arguments.length-1],index;
-  var port = chrome.tabs.connect(tab.id, {});
-
-  function sendBackCurrentUrl() {
-    port.postMessage({ action : "Url.shortUrl", url : tab.url });
-  }
-
-  var xhr = new XMLHttpRequest();
-  xhr.open( "GET", "http://is.gd/api.php?longurl=" + tab.url, false);
-  xhr.onerror = sendBackCurrentUrl;
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4) {
-      if (xhr.status == 200) {
-        port.postMessage({ action : "Url.shortUrl", url : xhr.responseText });
-      }else{
-        sendBackCurrentUrl();
-      }
-    }
-  }
-  xhr.send();
-}
-
 // notify new version
 // var appVersion = "0.3.3";
 // function checkFirstTime() {
---
+++ src/background/scripts/modules/shorturl.js
@@ -0,0 +1,22 @@
+function shortUrl(msg) {
+  var tab = arguments[arguments.length-1],index;
+  var port = chrome.tabs.connect(tab.id, {});
+
+  function sendBackCurrentUrl() {
+    port.postMessage({ action : "Url.shortUrl", url : tab.url });
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open( "GET", "http://is.gd/api.php?longurl=" + tab.url, false);
+  xhr.onerror = sendBackCurrentUrl;
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        port.postMessage({ action : "Url.shortUrl", url : xhr.responseText });
+      }else{
+        sendBackCurrentUrl();
+      }
+    }
+  }
+  xhr.send();
+}
