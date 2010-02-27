3ee4472f225360d7b3e994e99ffa6b6db45d4dc1
ZhangJinzhu <wosmvp@gmail.com>
2010-02-27 22:45:31 +0800

    下一頁,上一頁

--- src/shared/options.js
+++ src/shared/options.js
@@ -1,7 +1,7 @@
 var Option = (function() {
   var options = {
-    nextpattern : ['(下|后)一页','^\\s*Next\\s*$','^>$','^More$','(^(>>|››|»))|((»|››|>>)$)'],
-    previouspattern : ['(上|前)一页','^\\s*Prev(ious)?\\s*$','^<$','(^(<<|‹‹|«))|((<<|‹‹|«)$)'],
+    nextpattern : ['(下|后)一页','下一頁','^\\s*Next\\s*$','^>$','^More$','(^(>>|››|»))|((»|››|>>)$)'],
+    previouspattern : ['(上|前)一页','上一頁','^\\s*Prev(ious)?\\s*$','^<$','(^(<<|‹‹|«))|((<<|‹‹|«)$)'],
     disablesites : "",
     editor : "gvim -f"
   }
