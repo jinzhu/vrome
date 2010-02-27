de32f97c2192608311e67e23ffba8730f642ba87
ZhangJinzhu <wosmvp@gmail.com>
2010-02-27 23:08:20 +0800

    fix key-event

--- src/frontend/modules/key-event.js
+++ src/frontend/modules/key-event.js
@@ -129,7 +129,7 @@ var KeyEvent = (function() {
 	function exec(e) {
 		var key        = getKey(e);
 		var insertMode = /^INPUT|TEXTAREA|HTML$/i.test(e.target.nodeName);
-		if (/^<(Control|Alt|Shift)>$/.test(key)) return;
+		if (/^(Control|Alt|Shift)$/.test(key)) return;
 		currentKeys += key;
 
     // if vrome set disabled/pass the next, use Esc to enable it again.
