e3057d4ea6ae54b9ffd7c527da8d6d8630336bd1
Jinzhu <wosmvp@gmail.com>
2010-03-14 11:41:30 +0800

    cmd line lock

--- src/frontend/modules/cmd-line.js
+++ src/frontend/modules/cmd-line.js
@@ -1,15 +1,18 @@
 var CmdLine = (function() {
   var commands = [];
+  var CmdLineMode;
 
 	function add(/*String*/ command,/*Function*/ fun) {
     commands.push([command,fun]);
 	}
 
   function start() {
+    CmdLineMode = true;
     CmdBox.set({content : ''});
   }
 
   function exec() {
+    if (!CmdLineMode) return;
     /^(\S+)\s*(.*)$/.test(CmdBox.get().content);
     var cmd     = RegExp.$1;
     var arg     = RegExp.$2;
@@ -22,6 +25,7 @@ var CmdLine = (function() {
       }
     }
     if (matched.length == 1) return matched[0].call('',arg);
+    CmdLineMode = false;
   }
 
   return { start : start, exec : exec , add : add };
