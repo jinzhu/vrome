f3af331bfcd67b466ade7fa03c298cc6cd4f69fa
Jinzhu <wosmvp@gmail.com>
2011-12-27 23:09:24 +0800

    Add bing, twitter as default search engines

--- src/shared/options.js
+++ src/shared/options.js
@@ -5,7 +5,7 @@ var Option = (function() {
     disablesites : "",
     editor : "gvim -f",
     server_port: 20000,
-    searchengines : {"google":"http://www.google.com/search?q={{keyword}}", "yahoo":"http://search.yahoo.com/search?p={{keyword}}","wikipedia":"http://en.wikipedia.org/wiki/{{keyword}}","answers":"http://www.answers.com/main/ntquery?s={{keyword}}"},
+    searchengines : {"google":"http://www.google.com/search?q={{keyword}}", "yahoo":"http://search.yahoo.com/search?p={{keyword}}", "bing":"http://www.bing.com/search?q={{keyword}}", "wikipedia":"http://en.wikipedia.org/wiki/{{keyword}}","answers":"http://www.answers.com/main/ntquery?s={{keyword}}", "twitter":"https://twitter.com/search/{{keyword}}"},
     google_api_key : null
   }
 
