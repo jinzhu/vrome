92495ef1fe3187c74f2a9435c94028a2988d03da
hbt <hassenbentanfous@gmail.com>
2012-04-08 22:10:28 -0400

    -#132 adds server status in options page (online/offline)

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,13 @@
 function getLocalServerUrl() {
   return 'http://127.0.0.1:' + Option.get('server_port');
 }
+
+function checkServerStatus() {
+  $.ajax(getLocalServerUrl()).done(function() {
+    $('#server_status').attr('src', '/images/server_online.png');
+    $('#server_status').attr('alt', 'Server Online');
+  }).fail(function() {
+    $('#server_status').attr('src', '/images/server_offline.png');
+    $('#server_status').attr('alt', 'Server Offline. Run ./vrome');
+  })
+}
