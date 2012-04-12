f9d7b40b9288c7f7ea5dd0d90ef5bcac08d05fd1
hbt <hassenbentanfous@gmail.com>
2012-04-11 22:03:29 -0400

    -#134 adds automated tests for hints

--- src/tests/spec/HintsSpec.js
+++ src/tests/spec/HintsSpec.js
@@ -3,16 +3,107 @@ describe("Hints", function() {
   beforeEach(function() {
     });
 
-  it("should be able to play a Song", function() {
-    });
+  it("should display hintmode input", function() {
+    $("#linkHintsContainer").show();
+    $("#HTMLReporter").hide();
 
-  describe("when song has been paused", function() {
-    it("should indicate that the song is currently paused", function() {
-//      expect(Hint.testFalse()).toBeFalsy();
+    CancelKeyFunction();
+
+    expect(getCmd().css('display')).toBeUndefined()
+    Hint.start()
 
 
-    });
+    expect(getCmd().css('display')).toEqual('inline-block')
+    expect(getCmd().val()).toEqual('')
+  });
+
+  it("should select inputs", function() {
+    CancelKeyFunction();
+    Hint.start()
+    simulateKey('2')
+    expect(document.activeElement.id).toEqual('i2')
+
+    CancelKeyFunction();
+    Hint.start();
+    simulateKey('1')
+    simulateTyping('input');
+    expect(document.activeElement.id).toEqual('i1')
+    expect(document.activeElement.value).toEqual('input')
+    document.activeElement.value = ''
+
+    CancelKeyFunction();
 
   });
+
+  it("should open one link in a new tab", function() {
+    var wait = 500
+    CancelKeyFunction()
+
+
+
+    waits(2000);
+
+    closeOtherTabs(function(tab){
+      Hint.new_tab_start()
+      simulateKey('3')
+
+      setTimeout(function(){
+        chrome.tabs.query({
+          windowId: tab.windowId
+        }, function(tabs) {
+          expect(tabs.length).toEqual(2)
+          expect(tabs[1].url).toEqual($('#uri1').attr('href'))
+
+          closeOtherTabs(function(tab){
+            Hint.new_tab_start()
+            simulateTyping('vr')
+
+            setTimeout(function(){
+              chrome.tabs.query({
+                windowId: tab.windowId
+              }, function(tabs) {
+                expect(tabs.length).toEqual(2)
+                expect(tabs[1].url).toEqual($('#uri2').attr('href'))
+              })
+            }, wait)
+          })
+
+        })
+      }, wait);
+    })
+  });
+
+
+  it("should open multiple links in a new tab", function() {
+    var wait = 500
+    CancelKeyFunction()
+
+    waits(2000);
+
+    closeOtherTabs(function(tab){
+      Hint.multi_mode_start()
+      simulateKey('3')
+      simulateKey('4')
+
+      setTimeout(function(){
+        chrome.tabs.query({
+          windowId: tab.windowId
+        }, function(tabs) {
+          expect(tabs.length).toEqual(3)
+
+          reset();
+
+        })
+      }, wait);
+    })
+  })
+
+  function reset() {
+    closeOtherTabs(function(tab){
+      CancelKeyFunction()
+      $(".testContainer").hide();
+      $("#HTMLReporter").show();
+    })
+  }
 
 });
