dda330dcca83de511c36f9fe098e2f7ba23d6e0c
hbt <hassenbentanfous@gmail.com>
2012-07-02 00:33:11 -0400

    new reload code for chrome 20

--- utils/reload_extension.js
+++ utils/reload_extension.js
@@ -19,8 +19,11 @@ function reloadExtension()
       {
         chrome.send('reload', [myExtensionID]);
         // recent versions of chrome
-        reload_ext();
-        setTimeout('reload_ext()', 500);
+        var elem = document.getElementById(myExtensionID).getElementsByClassName('reload-link')[0]
+        var oEvent = document.createEvent( "MouseEvents" );
+        oEvent.initMouseEvent("click", true, true,window, 1, 1, 1, 1, 1, false, false, false, false, 0, elem);
+        elem.dispatchEvent( oEvent );
+
         console.log("RELOADING at " + new Date());
         latestVersion = xhr.responseText;
       }
