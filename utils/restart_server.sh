122da4b39a9950f0fad504d626b5e927843de813
Jinzhu <wosmvp@gmail.com>
2013-02-05 19:08:43 +0800

    no error message when restart server

--- utils/restart_server.sh
+++ utils/restart_server.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 pid=`lsof -t -i :20000`
-echo $pid
-kill -9 $pid
+kill -9 $pid > /dev/null 2>&1
 
 echo "restarting server..."
-nohup ./system/ruby/bin/vrome  >> /tmp/vrome_server.out 2>> /tmp/vrome_server.err < /dev/null &
+./system/ruby/bin/vrome
