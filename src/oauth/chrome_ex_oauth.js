0022bcd560bbd1ae19b6b708fb455204fe6e487c
Boris Petrov <boris_petrov@live.com>
2014-01-25 11:46:28 +0200

    Update chrome_ex_oauth.js

--- src/oauth/chrome_ex_oauth.js
+++ src/oauth/chrome_ex_oauth.js
@@ -1,8 +1,6 @@
-/**
- * Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
- * source code is governed by a BSD-style license that can be found in the
- * LICENSE file.
- */
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 /**
  * Constructor - no need to invoke directly, call initBackgroundPage instead.
@@ -269,8 +267,8 @@ ChromeExOAuth.initCallbackPage = function() {
   oauth.initOAuthFlow(function (token, secret) {
     background_page.chromeExOAuthOnAuthorize(token, secret);
     background_page.chromeExOAuthRedirectStarted = false;
-    chrome.tabs.getSelected(null, function (tab) {
-      chrome.tabs.remove(tab.id);
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.remove(tabs[0].id);
     });
   });
 };
