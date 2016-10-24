9a8b284beab3dafd7c0bc52b0dd5d6e5e04d183b
Boris Petrov <boris_petrov@live.com>
2016-10-24 11:46:36 +0300

    Fix initializing Chrome OAuth before page load

--- src/oauth/oauth.js
+++ src/oauth/oauth.js
@@ -1,9 +1,11 @@
-var oauth = ChromeExOAuth.initBackgroundPage({
-	'request_url' : 'https://www.google.com/accounts/OAuthGetRequestToken',
-	'authorize_url' : 'https://www.google.com/accounts/OAuthAuthorizeToken',
-	'access_url' : 'https://www.google.com/accounts/OAuthGetAccessToken',
-	'consumer_key' : 'anonymous',
-	'consumer_secret' : 'anonymous',
-	'scope' : 'https://www.googleapis.com/auth/urlshortener',
-	'app_name' : 'Vrome - URL Shortener',
-});
+window.onload = function() {
+    window.oauth = ChromeExOAuth.initBackgroundPage({
+        'request_url' : 'https://www.google.com/accounts/OAuthGetRequestToken',
+        'authorize_url' : 'https://www.google.com/accounts/OAuthAuthorizeToken',
+        'access_url' : 'https://www.google.com/accounts/OAuthGetAccessToken',
+        'consumer_key' : 'anonymous',
+        'consumer_secret' : 'anonymous',
+        'scope' : 'https://www.googleapis.com/auth/urlshortener',
+        'app_name' : 'Vrome - URL Shortener',
+    });
+};
